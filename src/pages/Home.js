3cc4b9d6f289c364ced6cb06f606b7f0ed23a332
rogeriocsl <carlosrog1414@hotmail.com>
2025-01-15 20:38:08 -0300

    Update Home page layout with MUI components and background image; add new dependencies in package.json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { Button, Typography, Box } from "@mui/material";
+import backgroundImage from '../assets/background.png';
 const Home = () => {
   return (
-    <div>
-      <h1>Bem-vindo ao Gerenciador de Vôlei</h1>
-      <p>Escolha uma opção abaixo:</p>
-      <Link to="/home-participante">Ir para Home do Participante</Link>
-    </div>
+    <Box
+      sx={{
+        height: "100vh",
+        backgroundImage: `url(${backgroundImage})`,  // Substitua pelo caminho da sua imagem
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        textAlign: "center",
+        color: "white",  // Cor do texto, pode ser ajustada conforme o fundo
+      }}
+    >
+      <Typography variant="h3" gutterBottom>
+        Bem-vindo ao Gerenciador de Vôlei
+      </Typography>
+      <Typography variant="h6" paragraph>
+        Escolha uma opção abaixo:
+      </Typography>
+      <Link to="/home-participante" style={{ textDecoration: "none" }}>
+        <Button variant="contained" color="primary">
+          Ir para Home do Participante
+        </Button>
+      </Link>
+    </Box>
   );
 };
 
