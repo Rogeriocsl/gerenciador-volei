2167aa7be9a9edf7926f6f03c9e9b8cd3c6c0c1e
rogeriocsl <carlosrog1414@hotmail.com>
2025-01-15 20:07:01 -0300

    Update .gitignore, modify deploy script in package.json, and add duplicate paragraph in HomeParticipante page

--- src/pages/HomeParticipante.js
+++ src/pages/HomeParticipante.js
@@ -5,6 +5,7 @@ const HomeParticipante = () => {
     <div>
       <h1>Bem-vindo à Home do Participante</h1>
       <p>Esta é a página dedicada aos participantes do Gerenciador de Vôlei.</p>
+      <p>Esta é a página dedicada aos participantes do Gerenciador de Vôlei.</p>
     </div>
   );
 };
